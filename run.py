0a5145b73c2370d8215c28588fc1b39f0b425fb0
Lucas Borges <lucasbborges2004@gmail.com>
2025-05-12 12:34:37 -0300

    Create templates and Crud
    Foi desenvolvido o Create do Crud e arquivo inicial do frontend (index.html)

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,6 +14,20 @@ class Tasks(db.Model):
 def index():
     tasks = Tasks.query.all()
     return render_template('index.html', tasks=tasks)
+
+@app.route('/create', methods=["POST"])
+def create_task():
+    description = request.form['description']
+
+    # Valida se a task já foi criada
+    exist_task = Tasks.query.filter_by(description=description).first()
+    if exist_task:
+        return 'Erro: Task já existe!', 400
+
+    new_task = Tasks(description = description)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == '__main__':
     with app.app_context():
